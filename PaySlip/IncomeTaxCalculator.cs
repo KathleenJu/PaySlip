4fdad742c14f66e36391de075a5350a1cfdd6cc3
Kathleen Jumamoy <kathleen.jumamoy@myob.com>
2018-06-15 08:39:16 +1200

    refactored income tax

---
+++ PaySlip/IncomeTaxCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaySlip.Kata
+{
+    public class IncomeTaxCalculator
+    {
+        private static readonly List<TaxRateInfo> listOfTaxRate = new List<TaxRateInfo>
+        {
+            new TaxRateInfo(0, 18200, 0, 0, 0),
+            new TaxRateInfo(18201, 37000, 18200, 0.19, 0),
+            new TaxRateInfo(37001, 87000, 37000, 0.325, 3572),
+            new TaxRateInfo(87001, 180000, 87000, 0.37, 19822),
+            new TaxRateInfo(180001, Double.PositiveInfinity, 180000, 0.45, 54232) //special case for this
+        };
+
+        public static double CalculateIncomeTax(int annualSalary)
+        {
+            var nonTaxableSalary = 18200;
+            var taxPerDollar = 0.0;
+            foreach (var taxRange in listOfTaxRate)
+            {
+                if (annualSalary >= taxRange.getMinimumSalary() && annualSalary <= taxRange.getMaximumSalary())
+                {
+                    var taxableSalary = annualSalary - taxRange.setNonTaxableSalary();
+                    var taxOnSalary = taxableSalary * taxRange.getTaxPerDollar();
+                    var incomeTax = Math.Round((taxOnSalary + taxRange.getExtraTax()) / 12);
+
+                    return incomeTax;
+                }
+            } //must be pure function, shouldnt use the field listoftaxrate
+
+            return 0;
+        }
+    }
+}
--- PaySlip/PaySlipManager.cs
+++ PaySlip/PaySlipManager.cs
@@ -6,16 +6,6 @@ namespace PaySlip.Kata
 {
     public class PaySlipManager
     {
-
-        private static readonly List<TaxRateInfo> listOfTaxRate = new List<TaxRateInfo>
-        {
-            new TaxRateInfo(0, 18200, 0, 0, 0),
-            new TaxRateInfo(18201, 37000, 18200, 0.19, 0),
-            new TaxRateInfo(37001, 87000, 37000, 0.325, 3572),
-            new TaxRateInfo(87001, 180000, 87000, 0.37, 19822)   ,
-            new TaxRateInfo(180001, Double.PositiveInfinity, 180000, 0.45, 54232) //special case for this
-        };
-
         public static int CalculateGrossIncome(int annualSalary)
         {
             var grossIncome = annualSalary / 12;
@@ -33,24 +23,6 @@ namespace PaySlip.Kata
             var superRatePercentage = superRate / 100;
             var super = grossIncome * superRatePercentage;
             return Math.Floor(super);
-        }
-
-        public static double CalculateIncomeTax(int annualSalary)
-        {
-            var nonTaxableSalary = 18200;
-            var taxPerDollar = 0.0;
-            foreach (var taxRange in listOfTaxRate)
-            {
-                if (annualSalary >= taxRange.getMinimumSalary() && annualSalary <= taxRange.getMaximumSalary())
-                {
-                    var taxableSalary = annualSalary - taxRange.setNonTaxableSalary();
-                    var taxOnSalary = taxableSalary * taxRange.getTaxPerDollar();
-                    var incomeTax = Math.Round((taxOnSalary + taxRange.getExtraTax()) / 12);
-
-                    return incomeTax;
-                }
-            }//must be pure function, shouldnt use the field listoftaxrate
-            return 0;
         }
 
         public static int CalculateNetIncome(int grossIncome, int incomeTax)
